8d81a673f15e034baa3c99e8fe5eef3cc62f3959
leodoneDev <leodone125@gmail.com>
2025-04-03 23:38:03 +0700

    main function implement

--- lib/main.dart
+++ lib/main.dart
@@ -172,7 +172,7 @@ final router = GoRouter(
     ),
   ],
 );
-asdasdfaDasdASDASasdfasdfasdfasdf
+asdasdfaDasdASDASasdfasdfa
 class AnimationSamples extends StatelessWidget {
   const AnimationSamples({super.key});
 
